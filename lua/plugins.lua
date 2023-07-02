58b67e5fa46bcc0f9927f50ff1a90fb95d9aa970
Quasar_0330 <95518809+Quasar-0330@users.noreply.github.com>
2023-07-02 12:56:31 +0900

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,1 +1,10 @@
-
+vim.cmd.packadd "packer.nvim"
+require("packer").startup(function()
+    use {"wbthomason/packer.nvim", opt = true}
+    use "itchyny/lightline.vim"
+    use "cocopon/iceberg.vim"
+    use "neoclide/coc.nvim"
+    use "cohama/lexima.vim"
+    use "bronson/vim-trailing-whitespace"
+    use "Yggdroot/indentLine"
+end)
