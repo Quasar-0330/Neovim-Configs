306618d509d57f071bd020ec0bbc1da805ecee45
Quasar_0330 <95518809+Quasar-0330@users.noreply.github.com>
2024-01-01 04:38:43 +0900

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,8 @@
 vim.cmd.packadd "packer.nvim"
 require("packer").startup(function()
     use {"wbthomason/packer.nvim", opt = true}
-    use "itchyny/lightline.vim"
-    use "joshdick/onedark.vim"
+    use "nvim-lualine/lualine.nvim"
+    use "rmehri01/onenord.nvim"
     use "neoclide/coc.nvim"
     use "cohama/lexima.vim"
     use "bronson/vim-trailing-whitespace"
